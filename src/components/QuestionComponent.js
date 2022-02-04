d1d06ee7d3ced4b10d84aec03f8fa00a16773d2c
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-04 20:08:49 +0000

    Created QUestionComponent

---
+++ src/components/QuestionComponent.js
@@ -0,0 +1,15 @@
+function QuestionComponent() {
+  return (
+    <div className="col-sm">
+    <h3 className="text-center">Question</h3>
+    <div className="p-3 mb-2 bg-light">
+        <div className="text-center">
+            <img className="mb-4 rounded img-fluid" src="https://i-want-to-study-engineering.org/figs/balances.png" alt="test"/>
+        </div>
+        <p>Three balances are supported on rigid pivots and connected by two flexible light strings as shown in the diagram above. Note that the centre balance operates upside down compared to the other two. What value of mass X will enable the system to balance?</p>
+    </div>
+  </div>    
+  )
+}
+
+export default QuestionComponent;
