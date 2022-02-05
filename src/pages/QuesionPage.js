84d657b81e5a637d09bae2c391d251e4492896af
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-05 22:43:45 +0000

    imported navbar compnent

--- src/pages/QuesionPage.js
+++ src/pages/QuesionPage.js
@@ -1,12 +1,16 @@
 import AnswerComponent from "../components/AnswerComponent";
 import HintComponent from "../components/HintComponent";
 import QuestionComponent from "../components/QuestionComponent";
+import NavbarComponent from "../components/NavbarComponent";
 
 function QuestionPage() {
   return (
     <div className="container">
-        <h1 className="text-center">Question Page</h1>
         <div className="row">
+          <NavbarComponent />
+        </div>
+        <h1 className="text-center">Question Page</h1>
+        <div className="row">            
             <QuestionComponent />
             <HintComponent />
         </div>
