6cd82a8858a81e8b0eac80b77c1aba21a95f625e
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-05 09:07:25 +0000

    firebase config into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa2v3Wb4zlbmLF9FkBX_DRsYYjpUOkLw8",
+  authDomain: "assignment219000170.firebaseapp.com",
+  databaseURL: "https://assignment219000170-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "assignment219000170",
+  storageBucket: "assignment219000170.appspot.com",
+  messagingSenderId: "1086176751748",
+  appId: "1:1086176751748:web:318904d0a3a934d6d1d2d1",
+  measurementId: "G-J9Q9NG2H65"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export const auth = firebase.auth()
+// export const firestore = getStorage();
+// export const db = firebase.database();
+
 
 ReactDOM.render(
   <React.StrictMode>
