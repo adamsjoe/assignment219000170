5d2e5d7391e3b7dcaffa5165b1c366a39a3a810c
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-05 22:43:32 +0000

    Boilerplate navbar component

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../icons/iCog-icon.svg';
+
+function NavbarComponent() {
+  return (
+    // <nav className='navbar navbar-light' style={{'backgroundColor': '#FFFFFF'}}>
+    <nav className='navbar navbar-expand-sm navbar-light bg-none'>
+      <div className="container-fluid ">
+        <div className='navbar-bramd'>
+          <img src={logo} alt='logo' width='60px'/>
+        </div>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Problem Index</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Problem Generator</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Log in or Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavbarComponent;
