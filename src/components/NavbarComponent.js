832abe66a4c70fae9a9559468cb3ad923e3e3011
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-05 23:18:07 +0000

    Changed navbar styles to allow more responsive

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,28 +1,21 @@
+import '../styles/Navbar.css';
 import React from 'react';
 import logo from '../icons/iCog-icon.svg';
 
 function NavbarComponent() {
   return (
-    // <nav className='navbar navbar-light' style={{'backgroundColor': '#FFFFFF'}}>
-    <nav className='navbar navbar-expand-sm navbar-light bg-none'>
+    <nav className='navbar navbar-default navbar-expand-lg headingLine'>
       <div className="container-fluid ">
-        <div className='navbar-bramd'>
+        <div className='navbar-header'>
           <img src={logo} alt='logo' width='50px'/>
         </div>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Problem Index</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Problem Generator</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Log in or Sign Up</a>
-            </li>
-          </ul>
+        <ul className="nav navbar-nav">
+          <li><a className="nav-link" href="#">Problem Index</a></li>
+          <li><a className="nav-link disabled" href="#">Problem Generator</a></li>
+          <li><a className="nav-link" href="#">Log in or Sign Up</a></li>
+        </ul>
         </div>
-      </div>
+      {/* </div> */}
     </nav>
   )
 }
