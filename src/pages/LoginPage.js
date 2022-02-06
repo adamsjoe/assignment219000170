04931db4a17800fb27338e75855f1c1880b8e6d3
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-06 21:15:54 +0000

    Placeholder for login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+function Login() {
+  return (
+   <h1 className="text-center">Login</h1>
+  );
+}
+
+export default Login;
