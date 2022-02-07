1ba48c62dbaa1e5a9a0aa2fbe833e9bef8075584
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-07 21:30:31 +0000

    Added code to show the column titles

--- src/components/HintComponent.js
+++ src/components/HintComponent.js
@@ -5,13 +5,30 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 function HintComponent() {
+    const firestore = firebase.firestore();
+    const storage = firebase.storage();
+    const collectionId = "Questions";
+    const questionId = "balances";
+  
+    const [title, setTitle] = useState("");
+    const [genColumnnTitle, setGenColumnTitle] = useState("");
+    const [specificProblemColumnTitle, setSpecificProblemColumnTItle] = useState("");
+
+    useEffect(async () => {
+      const snapshot = await firestore.collection(collectionId).doc(questionId).get();
+      const questionData = snapshot.data();
+      setTitle(questionData[questionId].balances.hintColumn.columnTitle)
+      setGenColumnTitle(questionData[questionId].balances.hintColumn.generalColumnTitle)
+      setSpecificProblemColumnTItle(questionData[questionId].balances.hintColumn.specificColumnTitle)
+    })
+
   return (
     <div className="col-sm">
-      <h3 className="text-center">Hints</h3>
+      <h3 className="text-center">{title}</h3>
       <div className="p-3 mb-2 bg-light">
           <div className="row">
               <div className="col-6 text-center">
-                  <p className='headerGeneral'>General</p>
+                  <p className='headerGeneral'>{genColumnnTitle}</p>
                   <div className="mb-5 d-grid gap-2 borderGeneral">
                       <button type="button" className="buttonGeneral">Primary</button>
                       <button type="button" className="buttonGeneral">Primary</button>                                    
@@ -26,7 +43,7 @@ function HintComponent() {
                   </div>                                                                
               </div>
               <div className="col-6 text-center">
-                  <p className='headerProblemSpecific'>Problem Specific</p>
+                  <p className='headerProblemSpecific'>{specificProblemColumnTitle}</p>
                   <div className="mb-5 d-grid gap-2 borderProblemSpecific">
                       <button type="button" className="buttonProblemSpecific">Success</button>
                       <button type="button" className="buttonProblemSpecific">Success</button>                                    
