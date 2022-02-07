ad0974b11f9b0d0a1a6d286f8a2c3565ca6eeccb
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-07 13:09:56 +0000

    Changed answer buttons style.

--- src/components/AnswerComponent.js
+++ src/components/AnswerComponent.js
@@ -42,12 +42,12 @@ function AnswerComponent() {
       <div className="p-3 mb-2 bg-light">
           <div className="row">
               <div className="col-sm d-grid gap-2">
-                  <button type="button" className="btn btn-secondary mb-2 p-4">Secondary</button>
-                  <button type="button" className="btn btn-secondary mb-2 p-4">Secondary</button>
+                  <button type="button" className="buttonAnswer">Secondary</button>
+                  <button type="button" className="buttonAnswer">Secondary</button>
               </div>
               <div className="col-sm d-grid gap-2">
-                  <button type="button" className="btn btn-secondary mb-2 p-4">Secondary</button>
-                  <button type="button" className="btn btn-secondary mb-2 p-4">Secondary</button>
+                  <button type="button" className="buttonAnswer">Secondary</button>
+                  <button type="button" className="buttonAnswer">Secondary</button>
               </div>                            
           </div>  
           <div className="row">
