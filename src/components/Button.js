8295ce9a6aa0de4eaddbcce03733723cdeff21f3
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-14 21:14:08 +0000

    moved to a custom button - still needs work

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { MathComponent } from 'mathjax-react'
 
 function Button(props) {
   const handleClick=()=>{
     // logic here…..
   };
   return (
-    <button className={props.className} onClick={handleClick}>
-       {props.label}
+    <button className={props.className} onClick={handleClick}>      
+        <MathComponent tex={props.label} />
     </button>
   );
 }
