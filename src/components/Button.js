187388527d6699fbee895cf43d87759e599616c9
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-15 18:33:05 +0000

    reused button component for answer and submit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { MathComponent } from 'mathjax-react'
 
 function Button(props) {
+  
+  // determine if this is an answer button by checking if the isSubmit prop is checked
+  let answerBtn;
+  if (!props.isSubmit) {
+    answerBtn = true
+  } else {
+    answerBtn = false
+  }
+
+  // need to handle if the button is regular text (ie no formula)
+
   const handleClick=()=>{
     // logic here…..
   };
-  return (
-    <button className={props.className} onClick={handleClick}>      
-        <MathComponent tex={props.label} />
+  
+  return (          
+    <button className={props.className} onClick={handleClick}>
+      {answerBtn === true ? <MathComponent tex={props.label} /> : props.label}
     </button>
   );
 }
