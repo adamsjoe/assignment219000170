f57b2daaea73c923f238aa30d681f1829f2cd38e
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-16 21:51:51 +0000

    Added a button for login - nothing more

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import '../styles/loginStyle.css';
 
 function Login() {
-  return (
-   <h1 className="text-center">Login</h1>
+   
+  return (   
+   <div className='mainContainer'>
+      <div className='centralColumn'>
+        
+        <button 
+          className='confetti-button' 
+          onClick={async () => {
+            const provider = new firebase.auth.GoogleAuthProvider();
+            const googleLogin = await firebase.auth().signInWithPopup(provider);
+
+
+            console.log(googleLogin);
+          }
+        }>Login With Google</button>
+      </div>
+    </div>
   );
 }
 
