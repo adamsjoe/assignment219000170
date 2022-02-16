0ad6d64a3866822655fca80c787a25176bdf97e2
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-16 21:51:00 +0000

    changed button className

--- src/components/AnswerComponent.js
+++ src/components/AnswerComponent.js
@@ -50,7 +50,7 @@ function AnswerComponent() {
 
         </div>
         <div className="col-sm text-center">
-          <Button className='btn-primary mb-2 p-4' label='CHECK MY ANSWER' isSubmit={true}/>
+          <Button className='btn-primary mb-2 p-4' label='CHECK MY ANSWER' isFormula={true}/>
         </div>
       </div>
     </div>     
