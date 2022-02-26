7b7d38d1af34b7b992b52fda6bda50ec932b7e2f
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-26 23:14:14 +0000

    Correcting a spelling mistake in the page name

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -2,18 +2,19 @@ import AnswerComponent from "../components/AnswerComponent";
 import HintComponent from "../components/HintComponent";
 import QuestionComponent from "../components/QuestionComponent";
 
-function QuestionPage() {
+function QuestionPage(props) {
+  console.log(">> ques", props)
   return (
     <div className="container">
         <h1 className="text-center">Balances problem</h1>
         <div className="row">            
-            <QuestionComponent />
+            <QuestionComponent image={props.image} text={props.text}/>
             <HintComponent />
         </div>
         <div className="row">
-            <AnswerComponent />
+            <AnswerComponent answersArray={props.answersArray} />
         </div>
-    </div>
+    </div> 
   )
 }
 
