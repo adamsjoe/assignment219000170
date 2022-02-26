f6db0e42fbd217551b92414742d484c0fb45a8d5
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-26 11:49:03 +0000

    changed document location

--- src/components/HintComponent.js
+++ src/components/HintComponent.js
@@ -8,8 +8,8 @@ import 'firebase/compat/storage';
 function HintComponent() {
     const firestore = firebase.firestore();
     // const storage = firebase.storage();
-    const collectionId = "Questions";
-    const questionId = "balances";
+    const collectionId = "Balances";
+    const documentId = "Hints"
   
     const [title, setTitle] = useState("");
     const [genColumnnTitle, setGenColumnTitle] = useState("");
@@ -17,12 +17,12 @@ function HintComponent() {
 
     useEffect(() => {
         const getFirebase = async () => {
-            const snapshot = await firestore.collection(collectionId).doc(questionId).get();
-            const questionData = snapshot.data();
+          const snapshot = await firestore.collection(collectionId).doc(documentId).get();
+          const questionData = snapshot.data(); 
 
-            setTitle(questionData[questionId].balances.hintColumn.columnTitle)
-            setGenColumnTitle(questionData[questionId].balances.hintColumn.generalColumnTitle)
-            setSpecificProblemColumnTItle(questionData[questionId].balances.hintColumn.specificColumnTitle)            
+          setTitle(questionData.sectionTitle)
+          setGenColumnTitle(questionData.generalColumnTitle)
+          setSpecificProblemColumnTItle(questionData.specificColumnTitle)            
         }
         getFirebase();
     },[firestore])    
@@ -30,35 +30,35 @@ function HintComponent() {
   return (
     <div className="col-sm">
       <h3 className="text-center">{title}</h3>
-      <div className="p-3 mb-2 bg-light">
+      <div className="p-3 mb-2 bg-light ">
           <div className="row">
               <div className="col-6 text-center">
-                  <p className='headerGeneral'>{genColumnnTitle}</p>
+                  <p className='headerGeneral'>{genColumnnTitle}</p>                  
                   <div className="mb-5 d-grid gap-2 borderGeneral">
-                      <Button className="buttonGeneral" label='Primary' />
+                      <Button className="buttonGeneral" label='VIDEO' />
                       <Button className="buttonGeneral" label='Primary' />
                   </div>
                   <div className="mb-5 d-grid gap-2 borderGeneral">
-                      <Button className="buttonGeneral" label='Primary' />
+                      <Button className="buttonGeneral" label='VIDEO' />
                       <Button className="buttonGeneral" label='Primary' />
                   </div>
                   <div className="mb-5 d-grid gap-2 borderGeneral">
-                      <Button className="buttonGeneral" label='Primary' />  
+                      <Button className="buttonGeneral" label='VIDEO' />  
                       <Button className="buttonGeneral" label='Primary' />
                   </div>                                                                
               </div>
               <div className="col-6 text-center">
                   <p className='headerProblemSpecific'>{specificProblemColumnTitle}</p>
                   <div className="mb-5 d-grid gap-2 borderProblemSpecific">
-                    <Button className="buttonProblemSpecific" label='Success' />  
+                    <Button className="buttonProblemSpecific" label='VIDEO' />  
                     <Button className="buttonProblemSpecific" label='Success' />                                
                   </div>
                   <div className="mb-5 d-grid gap-2 borderProblemSpecific">
-                    <Button className="buttonProblemSpecific" label='Success' />  
+                    <Button className="buttonProblemSpecific" label='VIDEO' />  
                     <Button className="buttonProblemSpecific" label='Success' />                                
                   </div>
                   <div className="mb-5 d-grid gap-2 borderProblemSpecific">
-                    <Button className="buttonProblemSpecific" label='Success' />  
+                    <Button className="buttonProblemSpecific" label='VIDEO' />  
                     <Button className="buttonProblemSpecific" label='Success' />                                
                 </div>                                                                
               </div>
