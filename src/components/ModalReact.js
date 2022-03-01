e1c94fc40ea0a00c2e9017817e896162a2344db7
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-03-01 21:13:57 +0000

    fixed modal for example usage.

--- src/components/ModalReact.js
+++ src/components/ModalReact.js
@@ -1,43 +1,29 @@
-import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import '../styles/react-bs.css';
 
-function ModalReact(props) {
-  const {showModal = false, onClose = ()=>{}} = props;
-  const [show, setShow] = useState(showModal);
-
-  React.useEffect(()=>{
-    setShow(showModal);
-  },[showModal]);
-
-  const handleClose = () => {
-   setShow(false);
-   // just to have custom function for modal close which will be used can be used in HintComponent maybe you want to perform somehting else after modal close.
-   typeof onClose === 'function' && onClose();
-  };
-  // const handleShow = () => setShow(true);
-
+function ModalReact({showModal = false, onClose = () =>{}}) {
   return (
-    <>
-      <Modal
-        show={show} 
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}        
-      >
+    <Modal
+    size="xl"
+    show={showModal} 
+    onHide={onClose}
+    backdrop="static"
+    keyboard={false}    
+    dialogClassName="videoPopup"    
+  >
 
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+    {/* <Modal.Header closeButton>
+      {/* <Modal.Title>Modal heading</Modal.Title> */}
+    {/* </Modal.Header> */}
+    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={onClose}>
+        Close
+      </Button>
+    </Modal.Footer>
+  </Modal>
+  )
 }
 
 export default ModalReact
