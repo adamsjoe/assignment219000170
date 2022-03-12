19c7e573ebf0c01e0c5249d5e53ffd4ca72154b3
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-03-12 00:36:32 +0000

    Added confused logic to the video popup - bug

--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -1,22 +1,45 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import '../styles/react-bs.css';
+import { useState } from 'react';
 
 function VideoModal({showModal = false, onClose = () =>{}, videoMessage, content, size}) {
+
+  const [confused, setConfused] = useState(false)
+
   return (
     <Modal
       size={size}
       show={showModal} 
       onHide={onClose}
+      onClose={()=>setConfused(false)}
       backdrop="static"
-      keyboard={false}    
-      // dialogClassName="videoPopup"    
+      keyboard={false}
     >
 
     <Modal.Body>
-      <video src={videoMessage} autoPlay></video> 
-      <br/>
-      {/* {content} */}
+      <video src={videoMessage} controls autoPlay></video> 
+      <div>
+        {confused ? (
+        <div>
+          What have you found confusing about this video?
+          <textarea className='confusedText' rows="2"></textarea>          
+          <Button className="confusedBtnSave">
+            Save
+          </Button>
+          <Button className="confusedBtnCancel" onClick={()=>setConfused(false)}>
+            Cancel
+          </Button>          
+        </div>
+        ) : (
+        <div>
+          <Button className="confusedBtn" onClick={()=>setConfused(true)}>
+            Confused?
+          </Button>
+        </div>
+      )}
+      </div>
+      
     </Modal.Body>
     <Modal.Footer>
       <Button variant="secondary" onClick={onClose}>
