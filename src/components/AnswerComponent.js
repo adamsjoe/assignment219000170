ee42c7670274c208ffe357ac5e39d06df843ceba
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-03-13 12:07:46 +0000

    get the "key" This will be used to update the count of clicked questions (well the start of it)

--- src/components/AnswerComponent.js
+++ src/components/AnswerComponent.js
@@ -8,28 +8,38 @@ function AnswerComponent(props) {
   let totalNoAnswers = props.totalAnswers;
   let answers = props.answersarray; 
   
-  const [valueA, setValue] = useState()
-  const [ansTrue, setAnsTrue] = useState("")
+  const [valueA, setValue] = useState() // make this mean something better
   const [showCongratsModal, setShowCongratsModal] = useState(false);
   const [showCongratsURL, setShowCongratsURL] = useState("")
   const [showWindowContent, setShowWindowContent] = useState("")
+  const [selectedAnswerGroup, setSelectedAnswerGroup] = useState("")
+  const [chosenCount, setChosenCount]= useState()
 
-  function handleChange(val) {
-    setValue(val);    
+  // console.log(">>",answers)
+  // function handleChange(val) {
+  //   // setValue(val);   
+  //  // alert(el)
+  //  alert ("you chose " + selectedAnswerGroup)
+  // }
+  
+  // const handleChange = (val,e) => {
+  //   alert(e.target.value)
+  // }
+
+  function updateChosenCount() {
+    // somehow update cloudstore when we have clicked the button with the right shit
   }
 
   function checkAnswer() {   
+    // alert ("you chose " + selectedAnswerGroup)
+    // alert ("the answer was picked " + chosenCount + " times")
+    setShowCongratsModal(true)
+    // set the selectedAnswer
     if (valueA === true) {
-      // alert("you got the answer correct")
-      setShowCongratsModal(true)
       setShowCongratsURL("https://firebasestorage.googleapis.com/v0/b/assignment219000170.appspot.com/o/videos%2Fcongrat_w3_s.mp4?alt=media&token=034ea1bc-b3e0-4b51-957f-854dae963896")
     } else if (valueA === false) {
-      // alert("Epic sadness.  Try again")
-      setShowCongratsModal(true)
       setShowWindowContent("Incorrect answer - please try again.")            
     } else {
-      // alert("select an answer first!")
-      setShowCongratsModal(true)
       setShowWindowContent("Please choose an answer before proceeding.")            
     }
   }
@@ -45,6 +55,7 @@ function AnswerComponent(props) {
   }
 
   answers.sort(compare);
+  // console.log(answers)
 
   // valueA === true ? checkAnswerBtnClass = 'correctAnswer mb-2 p-4' : checkAnswerBtnClass = 'wrongAnswer mb-2 p-4'
 
@@ -59,6 +70,8 @@ function AnswerComponent(props) {
             
             
             answers.map((answer, id) => {
+              
+              // console.log("XX ", answer )
 
               let formulaButton;
               let label = answer.text;
@@ -72,7 +85,8 @@ function AnswerComponent(props) {
               <input type='radio'
                     name='answer'
                     id={id}
-                    onChange={() => handleChange(answer.correct)}
+                    // onChange={() => handleChange(answer.correct)}                    
+                    onChange={() => {setValue(answer.correct); setSelectedAnswerGroup(answer.key); setChosenCount(answer.chosen)}}                    
                     value={answer.correct}
               />
               <label htmlFor={id}>{formulaButton === true ? <MathComponent tex={answer.text} /> : <div className='mathjaxFakery'>{answer.text}</div>}<span className='percentage'>{percentPicked}%</span></label>
@@ -86,7 +100,7 @@ function AnswerComponent(props) {
         </div>
         <div className="row">
           <div className="col-sm text-center">
-            <button className={'buttonCheck ' + (ansTrue === true ? 'correctAnswer' : '')}  onClick={checkAnswer}>CHECK MY ANSWER</button> <CheckAnswerModal showModal={showCongratsModal} onClose={() => setShowCongratsModal(false)} videoMessage={showCongratsURL} content={showWindowContent} size='med'/>
+            <button className='buttonCheck' onClick={checkAnswer}>CHECK MY ANSWER</button> <CheckAnswerModal showModal={showCongratsModal} onClose={() => setShowCongratsModal(false)} videoMessage={showCongratsURL} content={showWindowContent} size='med'/>
           </div>
         </div>
       </div>
@@ -95,4 +109,4 @@ function AnswerComponent(props) {
 }
 
 
-export default AnswerComponent;
+export default AnswerComponent; 
