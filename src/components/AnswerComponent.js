6e66e7572bb8d22fa6e440f5eba0e5193b48ab8d
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-03-14 21:56:29 +0000

    wip - need effect to work

--- src/components/AnswerComponent.js
+++ src/components/AnswerComponent.js
@@ -1,37 +1,70 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { MathComponent } from 'mathjax-react'
 import CheckAnswerModal from './CheckAnswerModal';
 import '../styles/radioStyles.css'
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
 
 function AnswerComponent(props) {
-  let totalNoAnswers = props.totalAnswers;
-  let answers = props.answersarray; 
-  
-  const [valueA, setValue] = useState() // make this mean something better
+  // let totalNoAnswers = props.totalAnswers;
+  // let answers = props.answersarray; 
+
+  const db = firebase.firestore();   
+  const firestore = firebase.firestore();   
+  const collectionId = "Questions";
+  const documentId = "balances";
+
+  const [correctAnswer, setCorrectAnswer] = useState() 
   const [showCongratsModal, setShowCongratsModal] = useState(false);
   const [showCongratsURL, setShowCongratsURL] = useState("")
   const [showWindowContent, setShowWindowContent] = useState("")
   const [selectedAnswerGroup, setSelectedAnswerGroup] = useState("")
   const [chosenCount, setChosenCount]= useState()
 
+  const [answers, setAnswers] = useState([]);
+
+  const [getAnswers, setTotalAnswers] = useState()
+
+  useEffect(() => {
+    const getFirebase = async () => {
+      const snapshot = await firestore.collection(collectionId).doc(documentId).get();
+      const questionData = snapshot.data();      
+
+      // now we add the answers and correct flag to our answers
+      const answerArr = [];
+      let selectedAnswers = 0 
+      Object.keys(questionData.balances.balances.answers).forEach(key => {        
+        const obj = questionData.balances.balances.answers[key]
+
+        // will need to know which document the database holds the chosen info, so let's add it to the array
+        obj['key'] = key
+        answerArr.push(obj);         
+
+        // count the number of times answers were "picked", (i.e. chosen)
+        selectedAnswers += questionData.balances.balances.answers[key].chosen
+      });
+
+      setAnswers(answerArr)    
+      setTotalAnswers(selectedAnswers)  
+
+    };
+    getFirebase();
+  },[firestore])
+
   function checkAnswer() {   
     setShowCongratsModal(true)
-    if (valueA === true) {
+    if (correctAnswer === true) {
       setShowCongratsURL("https://firebasestorage.googleapis.com/v0/b/assignment219000170.appspot.com/o/videos%2Fcongrat_w3_s.mp4?alt=media&token=034ea1bc-b3e0-4b51-957f-854dae963896")
-    } else if (valueA === false) {
+    } else if (correctAnswer === false) {
       setShowWindowContent("Incorrect answer - please try again.")            
     } else {
       setShowWindowContent("Please choose an answer before proceeding.")            
     }
 
     const updateFirestore = async () => {
-      const db = firebase.firestore();      
-      const collectionId = "Questions";
-      const documentId = "balances";
-      
+     
       console.log(selectedAnswerGroup);
       console.log(chosenCount);
       let newCount = chosenCount + 1
@@ -69,7 +102,7 @@ function AnswerComponent(props) {
 
   answers.sort(compare);
 
-  // valueA === true ? checkAnswerBtnClass = 'correctAnswer mb-2 p-4' : checkAnswerBtnClass = 'wrongAnswer mb-2 p-4'
+  // correctAnswer === true ? checkAnswerBtnClass = 'correctAnswer mb-2 p-4' : checkAnswerBtnClass = 'wrongAnswer mb-2 p-4'
 
   return (
     <div className="col-12">
@@ -85,16 +118,19 @@ function AnswerComponent(props) {
               let formulaButton;
               let label = answer.text;
               let timesPicked = answer.chosen
-              let percentPicked = Math.round((timesPicked / totalNoAnswers) * 100) // round this to a whole number - looks better than 1.22%
+              let percentPicked = Math.round((timesPicked / getAnswers) * 100) // round this to a whole number - looks better than 1.22%
+
+              // otherwise 0% will show as NaN!
+              percentPicked = parseInt(percentPicked) || 0
 
               label.includes('kg') ? formulaButton = true : formulaButton = false;
-                      
+
               return (
               <>
               <input type='radio'
                     name='answer'
                     id={id}
-                    onChange={() => {setValue(answer.correct); setSelectedAnswerGroup(answer.key); setChosenCount(answer.chosen)}}                    
+                    onChange={() => {setCorrectAnswer(answer.correct); setSelectedAnswerGroup(answer.key); setChosenCount(answer.chosen)}}                    
                     value={answer.correct}
               />
               <label htmlFor={id}>{formulaButton === true ? <MathComponent tex={answer.text} /> : <div className='mathjaxFakery'>{answer.text}</div>}<span className='percentage'>{percentPicked}%</span></label>
