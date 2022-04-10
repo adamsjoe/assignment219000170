f6272a57b112122b9655cf8192f185080d544b0b
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-04-10 10:16:45 +0100

    Chat modal parameterise :)

--- src/components/ChatModal.js
+++ src/components/ChatModal.js
@@ -12,16 +12,27 @@ import '../styles/chatroom.css'
 function ChatModal({showCModal = false, onClose = () =>{}, size, admin}) {
   const [text, setText] = useState('');
   const [userId, setUserId] = useState('');
+  
+  const [activeTab, setActiveTab] = useState('problem_s');
+  
   const [localMessages, setLocalMessages] = useState([]);
   const [localImage, setLocalImage] = useState(null);
 
   const firestore = firebase.firestore();
   const storage = firebase.storage();
   
+  let chatNames = [
+    'problem_s',
+    'spec_strat_balan_s',
+    'mom_s',
+    'spec_mom_s',
+    'gravity_s',
+    'spec_gravity_s'
+  ];
 
   useEffect(() => {
-    setUserId(firebase.auth()?.currentUser?.uid);
-    const query = firestore.collection('chats').where("video", "==", 'problem_s').orderBy("timestamp", "desc");      
+    setUserId(firebase.auth()?.currentUser?.uid);        
+    const query = firestore.collection('chats').where("video", "==", activeTab).orderBy("timestamp", "desc");      
     query.onSnapshot({
       next: (querySnapshot) => {
         let messages = []
@@ -31,12 +42,93 @@ function ChatModal({showCModal = false, onClose = () =>{}, size, admin}) {
         setLocalMessages(messages)
       }
     });
-  }, [firestore]);
+  }, [activeTab, firestore]);
 
   function getFirstNameFromGoogle() {
     var user = firebase.auth().currentUser;
     var names = user.displayName.split(' ')
     return names
+  }
+
+  function showChat() {
+    return (
+      <div>
+      <div className=''>
+        <div className='userLayout'>
+          {localMessages.map((localMessage) => (
+          <div className={`${userId}` === `${localMessage.uuid}` ? 'fromUserLayout userCurrentLayout' : 'fromUserLayout userOtherLayout'} >
+            <div className={`${userId}` === `${localMessage.uuid}` ? 'user userCurrent' : 'user userOther'}>
+              <p className='chatUser'>{`${userId}` === `${localMessage.uuid}` ? 'You' : localMessage.userName[0] } {localMessage.isAdmin === true ? '- ( Chat Admin )': ''}</p>
+              <p>{localMessage.content}</p>                                     
+              { localMessage?.image && localMessage.image.length > 0 && <img style={{width: '100%', height: 'auto', marginBottom: 24 }} src={localMessage.image} alt='chat' />  } 
+              <p className='chatTimestamp'><b>Sent:</b> {new Date(localMessage.timestamp).toLocaleTimeString()}, {new Date(localMessage.timestamp).toDateString() }</p>     
+              {(admin === true && localMessage.addedToFAQ === false) ? <button className='addFAQButton' onClick={async () => {
+                await firestore.collection('chats').doc(localMessage.mid).update({
+                  addedToFAQ: true
+                })
+              }}>Add to FAQ</button> : ''}                              
+            </div>
+          </div>)
+          )}
+        </div>
+        
+        <div className='inputAreaStyle'>
+          <form className='formInput' onSubmit={async (e) => {
+            e.preventDefault()
+            const timestamp = Date.now()
+            const content = text;
+            const uuid = firebase.auth().currentUser.uid
+            const type = 'StudentQuery' 
+            // const repliedTo = false
+            const isAdmin = admin
+            const addedToFAQ = false
+            const video = 'problem_s'
+            const userName = getFirstNameFromGoogle();
+            let image = ''      
+            if (localImage) {
+              const uniqueLocalImage = `${localImage.name}_${Math.random().toString(36)}`
+              const uploadTask = storage.ref(`/images/${uniqueLocalImage}`).put(localImage)
+              uploadTask.on('state_changed',
+              () => {},
+              () => {},
+              async () => {
+                const firebaseUrl = await storage.ref('images').child(uniqueLocalImage).getDownloadURL()
+                const message = {content, timestamp, uuid, type, addedToFAQ, video, userName, isAdmin, image: firebaseUrl}   
+                const docRef = await firestore.collection('chats').add(message)
+              })
+            } else {
+              const message = {content, timestamp, uuid, type, addedToFAQ, video, userName, isAdmin, image}
+              const docRef = await firestore.collection('chats').add(message)  
+            }
+
+            setText('')
+            setLocalImage(null)
+          }}>
+
+          <input className='input' type='text' value={text} onChange={(value) => {
+            setText(value.target.value)
+          }}/>
+          
+          <label htmlFor='file-upload' className='custom-file-upload'>
+            <i class="fa fa-cloud-upload"></i>Upload File
+          </label>
+          <input 
+            id='file-upload'
+            key={Date.now()}
+            style={{flex: 1 }}
+            type="file" 
+            onChange={(e) => {
+              const image = e.target.files[0]
+              setLocalImage(image)
+            }}
+          />
+            
+          <button className='buttonStyle'>Send</button>
+          </form>
+        </div>
+      </div>
+    </div>         
+    ) 
   }
 
   return (
@@ -49,7 +141,7 @@ function ChatModal({showCModal = false, onClose = () =>{}, size, admin}) {
     >
 
     <Modal.Body>
-      <Tabs>
+      <Tabs defaultIndex={0} onSelect={(index) => setActiveTab(chatNames[index])}>
         <TabList>
           <Tab>Gen Problem Solving</Tab> {/* (problem_s) */}
           <Tab>Spec Balances Problem</Tab> {/* (spec_strat_balan_s) */}
@@ -60,116 +152,27 @@ function ChatModal({showCModal = false, onClose = () =>{}, size, admin}) {
         </TabList>
 
         <TabPanel>
-          <p>
-            problem_s - remove this
-          </p>
-          <div>
-            <div className=''>
-              <div className='userLayout'>
-                {localMessages.map((localMessage) => (
-                <div className={`${userId}` === `${localMessage.uuid}` ? 'fromUserLayout userCurrentLayout' : 'fromUserLayout userOtherLayout'} >
-                  <div className={`${userId}` === `${localMessage.uuid}` ? 'user userCurrent' : 'user userOther'}>
-                    <p className='chatUser'>{`${userId}` === `${localMessage.uuid}` ? 'You' : localMessage.userName[0] } {localMessage.isAdmin === true ? '- ( Chat Admin )': ''}</p>
-                    <p>{localMessage.content}</p>                                     
-                    { localMessage?.image && localMessage.image.length > 0 && <img style={{width: '100%', height: 'auto', marginBottom: 24 }} src={localMessage.image} alt='chat' />  } 
-                    <p className='chatTimestamp'><b>Sent:</b> {new Date(localMessage.timestamp).toLocaleTimeString()}, {new Date(localMessage.timestamp).toDateString() }</p>     
-                    {(admin === true && localMessage.addedToFAQ === false) ? <button className='addFAQButton' onClick={async () => {
-                      await firestore.collection('chats').doc(localMessage.mid).update({
-                        addedToFAQ: true
-                      })
-                    }}>Add to FAQ</button> : ''}                              
-                  </div>
-                </div>)
-                )}
-              </div>
-              <div className='inputAreaStyle'>
-
-                <form className='formInput' onSubmit={async (e) => {
-                  e.preventDefault()
-                  const timestamp = Date.now()
-                  const content = text;
-                  const uuid = firebase.auth().currentUser.uid
-                  const type = 'StudentQuery' 
-                  // const repliedTo = false
-                  const isAdmin = admin
-                  const addedToFAQ = false
-                  const video = 'problem_s'
-                  const userName = getFirstNameFromGoogle();
-                  let image = ''      
-                  if (localImage) {
-                    const uniqueLocalImage = `${localImage.name}_${Math.random().toString(36)}`
-                    const uploadTask = storage.ref(`/images/${uniqueLocalImage}`).put(localImage)
-                    uploadTask.on('state_changed',
-                    () => {},
-                    () => {},
-                    async () => {
-                      const firebaseUrl = await storage.ref('images').child(uniqueLocalImage).getDownloadURL()
-                      const message = {content, timestamp, uuid, type, addedToFAQ, video, userName, isAdmin, image: firebaseUrl}   
-                      const docRef = await firestore.collection('chats').add(message)
-                    })
-                  } else {
-                    const message = {content, timestamp, uuid, type, addedToFAQ, video, userName, isAdmin, image}
-                    const docRef = await firestore.collection('chats').add(message)  
-                  }
-
-                  setText('')
-                  setLocalImage(null)
-                }}>
-
-                <input className='input' type='text' value={text} onChange={(value) => {
-                  setText(value.target.value)
-                }}/>
-                
-                <label htmlFor='file-upload' className='custom-file-upload'>
-                  <i class="fa fa-cloud-upload"></i>Upload File
-                </label>
-                <input 
-                  id='file-upload'
-                  key={Date.now()}
-                  style={{flex: 1 }}
-                  type="file" 
-                  onChange={(e) => {
-                    const image = e.target.files[0]
-                    setLocalImage(image)
-                  }}
-                />
-                  
-                <button className='buttonStyle'>Send</button>
-                </form>
-              </div>
-            </div>
-          </div>
-
+          {showChat()}
         </TabPanel>
         
-        <TabPanel>
-          <p>
-            spec_strat_balan_s
-          </p>
+        <TabPanel>          
+          {showChat()}
         </TabPanel>        
 
         <TabPanel>
-          <p>
-            mom_s
-          </p>
+          {showChat()}
         </TabPanel>
 
         <TabPanel>
-          <p>
-            spec_mom_s
-          </p>
+          {showChat()}
         </TabPanel>
 
         <TabPanel>
-          <p>
-            gravity_s
-          </p>
+          {showChat()} 
         </TabPanel>
 
         <TabPanel>
-          <p>
-            spec_gravity_s
-          </p>
+          {showChat()}
         </TabPanel>                                
       </Tabs>
 
